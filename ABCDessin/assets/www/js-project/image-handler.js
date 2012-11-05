75f53860f6dc80b3268cb98babee75927fc5354a
benjamin.chastanier@gmail.com <benjamin.chastanier@gmail.com@8ca1a3ef-a763-ef87-13b9-e87fa14d3d87>
2012-11-05 10:34:05 +0000

    Ajout des fichiers JS de gestion des ressources

---
+++ ABCDessin/assets/www/js-project/image-handler.js
@@ -0,0 +1,14 @@
+/*
+ * Fichier contenant la logique de gestion des images. 
+ * La librairie permettra de récupérer un flux son à partir de la description d'une lettre.
+ */
+
+/*
+ * Fonction permettant de récupérer le flux audio en fonction de la lettre décrite.
+ * Le format la lettre devra être a-z ou A-Z
+ * 
+ * @param letter : Lettre dont on veut la ressource audio (a-z; A-Z)
+ */
+getRessource = function (letter) {
+	
+}
---
+++ ABCDessin/assets/www/js-project/song-handler.js
@@ -0,0 +1,4 @@
+/*
+ * Fichier contenant la logique de gestion des sons. 
+ * La librairie permettra de récupérer l'image d'une lettre à partir de la description d'une lettre.
+ */
