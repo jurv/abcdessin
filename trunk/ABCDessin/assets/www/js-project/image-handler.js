e7cb6f29f20f8953af315d538c9175ee4088b363
benjamin.chastanier@gmail.com <benjamin.chastanier@gmail.com@8ca1a3ef-a763-ef87-13b9-e87fa14d3d87>
2012-11-06 12:33:40 +0000

    Premier archivage fichiers de gestion JS

--- trunk/ABCDessin/assets/www/js-project/image-handler.js
+++ trunk/ABCDessin/assets/www/js-project/image-handler.js
@@ -4,11 +4,60 @@
  */
 
 /*
- * Fonction permettant de récupérer le flux audio en fonction de la lettre décrite.
- * Le format la lettre devra être a-z ou A-Z
- * 
- * @param letter : Lettre dont on veut la ressource audio (a-z; A-Z)
+ * Classe permettant la gestion des ressources images.
  */
-getRessource = function (letter) {
+imageHandler = function() {
 	
-}
+	// Chemin vers le dossier de medias images
+	this.mediaPath = '/medias/images';
+	// Préfixe minuscule
+	this.prefixeMin = '-min';
+	// Préfixe majuscule
+	this.prefixeMaj = '-maj';
+	// Préfixe pointillé
+	this.prefixePt  = '-pt';
+	// Cursive
+	this.prefixeCursive = 'cursive';
+	// Extension image
+	this.extension = '.png';
+	
+	/*
+	 * Fonction permettant de récupérer l'image en fonction de la lettre décrite.
+	 * Le format la lettre devra être a-z ou A-Z
+	 * 
+	 * @param letter : Lettre dont on veut l'image (a-z; A-Z)
+	 */
+	this.getRessource = function (letter, isCursive) {
+		
+		var imagePath = '';
+		
+		// On test la validité du paramètre
+		if(letter && ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'))) {
+			// Le parametre passé est une lettre
+			console.log('image-handler : getRessource : Le paramètre est valide : ' + letter);
+			
+			// Initialisation du chemin de la ressource
+			imagePath = this.mediaPath + "/" + letter;
+			
+			// Majuscule ou Minuscule
+			if(letter >= 'a' && letter <= 'z')
+				imagePath += this.prefixeMin;
+			else if(letter >= 'A' && letter <= 'Z')
+				imagePath += this.prefixeMaj;
+			
+			// Gestion de la cursive
+			if(isCursive)
+				imagePath += '-cursive';
+			
+			// Ajout de l'extension
+			imagePath += this.extension;
+		}
+		else
+			console.log('image-handler : getRessource : Le paramètre n\'est pas valide.');
+		
+		console.log('image-handler : getRessource : Chemin de la ressource : "' + imagePath + '"');
+		return imagePath;
+	};
+	
+	
+};
